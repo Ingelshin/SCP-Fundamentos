c7ccb946cd52f03b0b891cb24319b4457a76f49f
Ingelshin Barraza Canchán <ingelnew.oro@gmail.com>
2019-12-28 15:28:09 +0000

    Data Binding con JSONModel

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -1,11 +1,26 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
 	"use strict";
-
+	
+	var oModel;
+	
 	return Controller.extend("com.inbc.scp-fundamentos.controller.Master", {
 		onInit: function () {
-
+			this._loadModels();
+			this._loadControls();
+		},
+		
+		_loadModels: function(){
+			oModel = new JSONModel();
+			oModel.loadData("mockdata/week.json");
+		},
+		
+		_loadControls: function(){
+			var oTable = this.byId("tblWeekend");
+			oTable.setModel(oModel);
+			oTable.bindRows("/week");
 		}
 	});
 });
