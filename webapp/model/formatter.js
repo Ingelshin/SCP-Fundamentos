8cb06bb8cc6bb853b8d926511dc2021cc107dc08
Ingelshin Barraza Canchán <ingelnew.oro@gmail.com>
2020-05-06 00:20:46 +0000

    updated

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	
-], function () {
+	"sap/ui/core/MessageType",
+	"sap/ui/core/IndicationColor"
+], function (MessageType, IndicationColor) {
 	"use strict";
 
 	return {
@@ -9,11 +10,26 @@ sap.ui.define([
 			var sValue;
 			switch(iState) {
 				case "0":
-					sValue = "Indication01";
+					sValue = IndicationColor.Indication01;
 					break;
 				case "1":
-					sValue = "Error";
+					sValue = MessageType.Error;
 					break;
+			}
+			return sValue;
+		},
+		
+		setColorRow: function(oContext){
+			var oDataHelp = this.getOwnerComponent().getModel("helpModel");
+			var sValue;
+			if (oContext.Stat2  === '09'){
+				sValue = MessageType.Success;
+			}else if (!oDataHelp.includes(oContext.Orgeh)){
+				sValue = IndicationColor.Indication05;             
+			}else if (oContext.Estex === "0"  || oContext.Estex === "4"){
+				sValue = MessageType.Error;
+			}else {
+				sValue = MessageType.None;
 			}
 			return sValue;
 		}
